4bb86bc4d589afd9f0719e61488d56b0619ef03a
Suareguen <adrian.suarez.monzon@gmail.com>
2023-03-27 19:17:00 +0100

    Project init
    General:
    - Download all packages.
    - Controllers, models, routes and database folders created.
    - index.js created in database folder, routes folder and general index.js

---
+++ api/routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+
+//Here the routes
+
+
+module.exports = router
---
+++ database/models.js
@@ -0,0 +1,15 @@
+//Add models here, for example:
+//const User = require('./models/user')
+
+
+
+function addRelationsToModels() {
+	try {
+        //Add relations here
+		console.log('Relations added to all models')
+	} catch (error) {
+		throw error
+	}
+}
+
+module.exports = addRelationsToModels
