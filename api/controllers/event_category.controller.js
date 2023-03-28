e62e024e52f803215cb7827a5249b29fe38135c0
Almacrh <Alma_Cruz_Hernandez@outlook.com>
2023-03-28 15:02:49 +0000

    methods for product controller added, routes added and tested. Changed datatypes decimal to allow 2 decimal number

--- api/controllers/event_category.controller.js
+++ api/controllers/event_category.controller.js
@@ -52,7 +52,7 @@ async function createCategory(req, res) {
     try {
         const category = await EventCategory.create(req.body)
         if (category) {
-            return res.status(200).json({ message: 'Category created', event: category })
+            return res.status(200).json({ message: 'Category created', category: category })
 
         } else {
             return res.status(404).send('Cannot create category')
