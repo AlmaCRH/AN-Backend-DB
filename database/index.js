310718a3fc5f7e67bcd2c381d358f68803248f77
Adrián <103899316+Suareguen@users.noreply.github.com>
2023-03-31 09:13:13 +0100

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
 
 // Connect to PostgreSQL creating a new sequelize instance
-const sequelize = new Sequelize(process.env.DIALECT+'://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME,{
+const connection = new Sequelize(process.env.DIALECT+'://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME,{
     dialect: process.env.DIALECT,
     port: process.env.DB_PORT,
     logging:false
